1f04a69861b17411aafad5c8b1cbe8edcb13c903
Daniel R <danieliusraudo@gmail.com>
2026-03-24 17:01:24 +0200

    add assemblies for each layer

---
+++ src/FulfillmentTracker.Harness/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+
+namespace FulfillmentTracker.Harness; 
+internal class Program {
+    /// <summary>
+    /// Challenge harness
+    /// </summary>
+    /// <param name="auth">Authentication token (required)</param>
+    /// <param name="endpoint">Problem server endpoint</param>
+    /// <param name="name">Problem name. Leave blank (optional)</param>
+    /// <param name="seed">Problem seed (random if zero)</param>
+    /// <param name="rate">Inverse order rate (in milliseconds)</param>
+    /// <param name="min">Minimum pickup time (in seconds)</param>
+    /// <param name="max">Maximum pickup time (in seconds)</param>
+    static async Task Main(string[] args) {
+
+
+        Console.WriteLine("Hello, World!");
+    }
+}
